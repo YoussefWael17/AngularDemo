c3fb33fe9aed1b159c2ba732af1371e5be6968b2
YoussefWael17 <youssef.wael9906@gmail.com>
2025-03-12 18:11:23 +0200

    add skills , portflio , footer Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PortoComponent } from './porto/porto.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , HeroSectionComponent],
+  imports: [RouterOutlet , HeroSectionComponent , AboutMeComponent , SkillsComponent , PortoComponent , FooterComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/porto/porto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-porto',
+  imports: [],
+  templateUrl: './porto.component.html',
+  styleUrl: './porto.component.css'
+})
+export class PortoComponent {
+
+}
